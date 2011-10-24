bc7e30e42804209438034ed5ba2394f1827d478e
nathancox <me@nathan.net.nz>
2011-10-25 10:20:06 +1300

    Added noConflict

--- javascript/showhidefield.js
+++ javascript/showhidefield.js
@@ -1,30 +1,35 @@
-(function($) {
-	$('.showhide select').livequery(function() {
-		var select = $(this);
+jQuery.noConflict();
+(function($) { 
+  $(function() {
+  
+		$('.showhide select').livequery(function() {
+			var select = $(this);
 
-		select.change(function() {
-			var selectVal = select.val();
-			$('option', this).each(function() {
-				var value = this.value;
-				if (value != '') {
-					var field = $('#'+value);
-					
-					if (field.length == 0) {
-						field = $('#'+value + 'ID');
-					}
+			select.change(function() {
+				var selectVal = select.val();
+				$('option', this).each(function() {
+					var value = this.value;
+					if (value != '') {
+						var field = $('#'+value);
+						
+						if (field.length == 0) {
+							field = $('#'+value + 'ID');
+						}
 
-					if (field.length == 1) {
-						if (value == selectVal) {
-							field.css('display', 'block');
-						} else {
-							field.css('display', 'none');
+						if (field.length == 1) {
+							if (value == selectVal) {
+								field.css('display', 'block');
+							} else {
+								field.css('display', 'none');
+							}
 						}
 					}
-				}
+				});
 			});
+			
+			select.trigger('change');
+			
 		});
-		
-		select.trigger('change');
-		
-	});
+	
+  });
 })(jQuery);
