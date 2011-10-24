92d0ee03558e92f635f818d7b785bd9530bf4251
nathancox <me@nathan.net.nz>
2011-10-25 10:19:48 +1300

    Now requires SS's jQuery

--- code/ShowHideField.php
+++ code/ShowHideField.php
@@ -4,6 +4,7 @@ class ShowHideField extends DropdownField {
 
 
 	static function include_files() {
+		Requirements::javascript(THIRDPARTY_DIR.'/jquery/jquery.js');
 		Requirements::javascript(THIRDPARTY_DIR.'/jquery-livequery/jquery.livequery.js');    
 		Requirements::javascript('showhidefield/javascript/showhidefield.js');
 	}
